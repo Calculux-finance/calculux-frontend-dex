64fa5cea3be35d967a66fbc4130d4f1f6a56df89
bigchiano <bigchiano@protonmail.com>
2021-12-28 13:46:51 +0200

    updating wallet config

--- src/connectors/index.ts
+++ src/connectors/index.ts
@@ -12,7 +12,7 @@ const NETWORK_URL = process.env.REACT_APP_NETWORK_URL
 const FORMATIC_KEY = process.env.REACT_APP_FORTMATIC_KEY
 const PORTIS_ID = process.env.REACT_APP_PORTIS_ID
 
-export const NETWORK_CHAIN_ID: number = parseInt(process.env.REACT_APP_CHAIN_ID ?? '1')
+export const NETWORK_CHAIN_ID: number = parseInt(process.env.REACT_APP_CHAIN_ID || '56')
 
 if (typeof NETWORK_URL === 'undefined') {
   throw new Error(`REACT_APP_NETWORK_URL must be a defined environment variable`)
@@ -29,17 +29,17 @@ export function getNetworkLibrary(): Web3Provider {
 }
 
 export const injected = new InjectedConnector({
-  supportedChainIds: [56, 97],
+  supportedChainIds: [56],
 })
 
 export const bsc = new BscConnector({ supportedChainIds: [56] })
 
 // mainnet only
 export const walletconnect = new WalletConnectConnector({
-  rpc: { 1: NETWORK_URL },
-  bridge: 'https://bridge.walletconnect.org',
+  rpc: { [NETWORK_CHAIN_ID]: NETWORK_URL },
+  // bridge: 'https://bridge.walletconnect.org',
   qrcode: true,
-  pollingInterval: 15000,
+  // pollingInterval: 12000,
 })
 
 // mainnet only
